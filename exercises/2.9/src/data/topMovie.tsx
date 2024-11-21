75a219c6ad4900a8905dab36fe23fbfda66b0b90
Georges Nodari <ngeorges.dev@gmail.com>
2024-11-21 13:36:43 +0100

    new:ex2.9+

---
+++ exercises/2.9/src/data/topMovie.tsx
@@ -0,0 +1,59 @@
+import IMovie from "../types";
+
+const myTopMovies: IMovie[] = [
+  {
+    title: "The Dark Knight",
+    director: "Christopher Nolan",
+    duration: 152,
+    link: "https://www.imdb.com/title/tt0468569/",
+    budget: 185000000,
+    description:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+  },
+
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    director: "Peter Jackson",
+    duration: 201,
+    link: "https://www.imdb.com/title/tt0167260/",
+    budget: 94000000,
+    description:
+      "Gandalf and Aragorn lead the World" +
+      "'s" +
+      "greatest armies to victory against the Dark Lord Sauron.",
+  },
+
+  {
+    title: "Inception",
+    director: "Christopher Nolan",
+    duration: 148,
+    link: "https://www.imdb.com/title/tt1375666/",
+    budget: 160000000,
+    description:
+      "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+  },
+
+  {
+    title: "The Matrix",
+    director: "Lana Wachowski, Lilly Wachowski",
+    duration: 136,
+    link: "https://www.imdb.com/title/tt0133093/",
+    budget: 63000000,
+    description:
+      "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
+  },
+
+  {
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    duration: 169,
+    link: "https://www.imdb.com/title/tt0816692/",
+    budget: 165000000,
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity" +
+      "'s" +
+      "survival.",
+  },
+];
+
+export default myTopMovies;
